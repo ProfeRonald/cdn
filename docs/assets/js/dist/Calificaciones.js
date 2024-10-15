178bbe9c3edccd2ff39f2a1833654d4184b8c25d
ProfeRonald <r.ronald@gmail.com>
2024-10-15 18:16:37 -0400

    Asistencia diaria

--- docs/assets/js/dist/Calificaciones.js
+++ docs/assets/js/dist/Calificaciones.js
@@ -794,7 +794,7 @@ $(document).ready(function () {
   })
     .done(function (ides) {
       console.log(ides);
-      if(ides == null || ides['e'] == 1){
+      if(ides['data'] != null && ides['e'] == 1){
         $('#msjasistencia').html(ides['data']);
       }else if(ides['data'] != null){
       $.each(ides['data'], function (id, estado) {

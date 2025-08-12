ba481de403614b1816dd5fb03c1ec587d37a1922
ProfeRonald <r.ronald@gmail.com>
2025-08-12 12:10:18 -0400

    Error ruleta

--- docs/assets/js/dist/Calificaciones.js
+++ docs/assets/js/dist/Calificaciones.js
@@ -1098,22 +1098,6 @@ $(document).ready(function () {
     $('#slidecontainer').removeAttr('display');
     $('#slidecontainer').hide('slow');
   });
-
-  if(local > 1){
-
-  const ruletawebfirebaseConfig = {
-    apiKey: "AIzaSyA9eJxcrKP8r4YuteGpfvQRTQxdj6ORqFg",
-    authDomain: "escuelard.edu.do",
-    databaseURL: "https://web-escuelard-default-rtdb.firebaseio.com",
-    projectId: "web-escuelard",
-    storageBucket: "web-escuelard.appspot.com",
-    messagingSenderId: "948522304281",
-    appId: "1:948522304281:web:4ccbd164a1dac6ddf03b88"
-    };
-
-  
-      const ruletaweb = firebase.initializeApp(ruletawebfirebaseConfig, "ruletaweb");
-  }
   
   $(document).on('click', '.iconHerramienta', function () {
 
---
+++ docs/assets/js/dist/RuletaWeb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  const ruletawebfirebaseConfig = {
+    apiKey: "AIzaSyA9eJxcrKP8r4YuteGpfvQRTQxdj6ORqFg",
+    authDomain: "escuelard.edu.do",
+    databaseURL: "https://web-escuelard-default-rtdb.firebaseio.com",
+    projectId: "web-escuelard",
+    storageBucket: "web-escuelard.appspot.com",
+    messagingSenderId: "948522304281",
+    appId: "1:948522304281:web:4ccbd164a1dac6ddf03b88"
+    };
+
+  
+      const ruletaweb = firebase.initializeApp(ruletawebfirebaseConfig, "ruletaweb");
+  
+
+});

8eaa6073548fd0926722151bc3ccf8bf83a79207
ProfeRonald <r.ronald@gmail.com>
2025-11-22 20:15:57 -0400

    Destacados

--- docs/assets/js/dist/App.js
+++ docs/assets/js/dist/App.js
@@ -15,7 +15,7 @@ if ($('#pwa-nav-bar').length === 0) {
         #pwa-nav-bar {
             position: fixed;
             top: 0;
-            left: 40%;
+            left: 50%;
             transform: translateX(-50%);
             display: flex;
             gap: 30px;
--- docs/assets/js/dist/Calificaciones.js
+++ docs/assets/js/dist/Calificaciones.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
   var notas_gc = $("#datos_js").attr("notas_gc");
   var datetype = $("#datos_js").attr("datetype");
   var rpp = Number($("#datos_js").attr("rpp"));
-
+  var mesp = $("#datos_js").attr("mesp");
 
 function BotonesRecuperacionPedagogica(){
 
@@ -1239,7 +1239,7 @@ function BotonesRecuperacionPedagogica(){
     $.ajax({
       method: "POST",
       url: "up.php?op=" + herr,
-      data: {titulo_asignaturamf: titulo_asignaturamf, listaruleta: listaruleta, titulo_grupo: titulo_grupo, id_asignaturamf: id_asignaturamf, id_grupo: id_grupo, year_1: year_1, year_2: year_2, lmeritorio: lmeritorio, cmeritorios: cmeritorios, tperiodos_escuela: tperiodos_escuela, id_asignaturamf: id_asignaturamf, incluir_grado: incluir_grado, grado_grupo: grado_grupo, imeses: imeses, kmeses: kmeses, archivoxlsx: $('#archivoxlsx').val()}
+      data: {titulo_asignaturamf: titulo_asignaturamf, listaruleta: listaruleta, titulo_grupo: titulo_grupo, id_asignaturamf: id_asignaturamf, id_grupo: id_grupo, year_1: year_1, year_2: year_2, lmeritorio: lmeritorio, cmeritorios: cmeritorios, tperiodos_escuela: tperiodos_escuela, id_asignaturamf: id_asignaturamf, incluir_grado: incluir_grado, grado_grupo: grado_grupo, imeses: imeses, kmeses: kmeses, mesp: mesp, archivoxlsx: $('#archivoxlsx').val()}
     })
       .done(function (cont) {
         

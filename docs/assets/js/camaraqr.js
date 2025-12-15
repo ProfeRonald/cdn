03145cb6efcacff089110be09240add285fcdf81
ProfeRonald <r.ronald@gmail.com>
2025-12-15 10:08:10 -0400

    Camera QR

--- docs/assets/js/camaraqr.js
+++ docs/assets/js/camaraqr.js
@@ -86,6 +86,7 @@ function read(a){
     }else if(a != ''){
      // $('head').append('<meta http-equiv="refresh" content="5; url=http://localhost/Dropbox/adecuacion/sesion.php?op=SesionQR" />');
       document.cookie = 'sesionqr=' + a;
+      document.getElementById("outdiv").innerHTML = '<i class="fa fa-spinner fa-spin fa-3x"></i>';
       var urlerd = document.getElementById("datos_js").getAttribute('urlerd');
       if(urlerd == null || urlerd == undefined || urlerd == ''){
         urlerd = 'https://escuelard.edu.do';
@@ -108,7 +109,7 @@ function success(stream){
     setTimeout(captureToCanvas, 500);
 }
 		
-function error(error){
+function Verror(error){
     gUM=false;
     return;
 }
@@ -169,7 +170,7 @@ function setwebcam2(options){
             then(function(stream){
                 success(stream);
             }).catch(function(error){
-                error(error)
+                Verror(error);
             });
     }
     else

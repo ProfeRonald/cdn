4090e93d4e14e9f82dd44c72e29d244ff6845ca3
ProfeRonald <r.ronald@gmail.com>
2026-02-07 18:11:06 -0400

    ListaCurso Excel

--- docs/assets/js/dist/Psicologia.js
+++ docs/assets/js/dist/Psicologia.js
@@ -65,22 +65,14 @@ var idlp = Number(document.cookie.replace(
             exportOptions: {
               columns: ':not(.no-export)',
               format: {
-        body: function (data, row, column, node) {
-          // 1. Obtenemos el valor "crudo" del atributo directamente del HTML
-          var valorAtributo = node.getAttribute('data-export');
-
-          console.log("Fila: " + row + " | Col: " + column + " | Atributo: " + valorAtributo);
-
-          // 2. Si existe, lo devolvemos (esto ignora todo elHTML de la celda)
-          if (valorAtributo !== null) {
-            return valorAtributo;
-          }
-
-          // 3. Si no tiene el atributo, limpiamos etiquetas HTML del contenido 'data'
-          // Esto sirve para el resto de las celdas normales
-          return data.replace(/<[^>]*>?/gm, '').trim();
-        }
-      }
+                body: function ( data, row, column, node ) {
+                  var exportText = $(node).attr('data-export');
+                    if (exportText) {
+                      return exportText;
+                    }
+                  return $(node).text().trim();
+                }
+              }
             }
           }
         ],

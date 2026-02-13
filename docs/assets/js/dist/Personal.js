ef63b28b05f395ab53a6b72d27771b83ca783be7
ProfeRonald <r.ronald@gmail.com>
2026-02-12 22:53:21 -0400

    Reportes coordinacion

--- docs/assets/js/dist/Personal.js
+++ docs/assets/js/dist/Personal.js
@@ -13,6 +13,56 @@ $(document).ready(function() {
       }
 
     var tablep = $('#TablaPersonal').DataTable({
+      dom: 'lBfrtip',
+        buttons: [
+          {
+            extend: 'excelHtml5',
+            text: '<i class="fa fa-file-excel-o"></i> Descargar lista del personal',
+            title: 'Lista del personal',
+            filename: 'lista_personal',
+            className: 'btn btn-success btn-sm rounded-pill px-3',
+            footer: true,
+            exportOptions: {
+              columns: ':not(.no-export)',
+              footer: true,
+              format: {
+                body: function ( data, row, column, node ) {
+                  var exportText = $(node).attr('data-export');
+                    if (exportText) {
+                      return exportText.replace(/<br\s*\/?>/gi, '\n');
+                    }
+                  return $(node).text().trim();
+                },
+                header: function ( data, column, node ) {
+                  var exportText = $(node).attr('data-export');
+                    if (exportText) {
+                      return exportText.replace(/<br\s*\/?>/gi, '\n').trim();
+                    }
+                  return $(node).text().replace(/<br\s*\/?>/gi, '\n').trim();
+                },
+                footer: function(data, column, node) {
+                if (node) {
+                        var attr = node.getAttribute('data-export');
+                        if (attr) return attr.replace(/<br\s*\/?>/gi, '\n').trim();
+                    }
+                    // Si no hay atributo, limpiamos el HTML
+                    return data ? data.replace(/<[^>]*>?/gm, '').trim() : '';
+        }
+              }
+            },
+            customize: function(xlsx) {
+    var sheet = xlsx.xl.worksheets['sheet1.xml'];
+    
+    // Buscamos las celdas que contienen el carácter de salto de línea
+    $('row c', sheet).each(function() {
+        if ($('is t', this).text().indexOf('\n') !== -1) {
+            // El estilo '25' es: Alineado a la izquierda + Wrap Text
+            $(this).attr('s', '55'); 
+        }
+    });
+}
+          }
+        ],
       "bAutoWidth": false,
       "responsive": true,
         "columnDefs": [ {

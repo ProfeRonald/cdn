917119750cd0ce73da34a0563be5b12ef2193cd6
ProfeRonald <r.ronald@gmail.com>
2026-09-11 17:59:18 -0400

    build: publish latest CDN bundle

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,22 @@
-const CACHE_NAME = 'escuelard-v4-shell-1';
+const CACHE_NAME = 'escuelard-v4-shell-2';
 const APP_SHELL = ['./'];
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', 'https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+firebase.initializeApp({
+  apiKey: 'AIzaSyA9eJxcrKP8r4YuteGpfvQRTQxdj6ORqFg',
+  authDomain: 'web-escuelard.firebaseapp.com',
+  projectId: 'web-escuelard',
+  messagingSenderId: '948522304281',
+  appId: '1:948522304281:web:4ccbd164a1dac6ddf03b88',
+});
+firebase.messaging().setBackgroundMessageHandler((payload) => {
+  const notification = payload.notification || {};
+  return self.registration.showNotification(notification.title || 'EscuelaRD', {
+    body: notification.body || 'Tienes una nueva notificación.',
+    icon: './favicon.ico',
+    data: payload.data || {},
+  });
+});
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
